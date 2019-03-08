6ba5994523f500de4b34e2cb821ab5265c4b416e
TheBritishBrotherhood <TheBritishBrotherhood@users.noreply.github.com>
2019-03-08 23:46:37 +0000

    Cleaned your code.

--- gamz-voice/client.lua
+++ gamz-voice/client.lua
@@ -1,20 +1,15 @@
-local Keys = {
-	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}
+--Functions-
 
+function Marker(type, x, y, z, voiceS)
+     DrawMarker(type, x, y, z - 1.2, 0.0, 0.0, 0.0, 0, 0.0, 0.0, voiceS, voiceS, 1.0, r, g, b, a, false, true, 2, false, false, false, false)
+end
 
+--Local variables--
 local r,g,b,a = 166,166,255,255 -- rgba color
 
 local voice = {default = 5.0, shout = 12.0, whisper = 1, current = 0}
 
+--Event that triggers when you enter the game--
 AddEventHandler('onClientMapStart', function()
 	if voice.current == 0 then
 		NetworkSetTalkerProximity(voice.default)
@@ -25,11 +20,12 @@ AddEventHandler('onClientMapStart', function()
 	end
 end)
 
+--Voip control--
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
         local coords = GetEntityCoords(PlayerPedId())
-        if IsControlJustPressed(0, Keys['H']) and IsControlPressed(0, Keys['LEFTSHIFT']) then
+        if IsControlJustPressed(0, 21) and IsControlPressed(0, 74) then
             voice.current = (voice.current + 1) % 3
             if voice.current == 0 then
                 NetworkSetTalkerProximity(voice.default)
@@ -39,7 +35,7 @@ Citizen.CreateThread(function()
                 NetworkSetTalkerProximity(voice.whisper)
             end
         end
-        if IsControlPressed(0, Keys['H']) and IsControlPressed(0, Keys['LEFTSHIFT']) then
+        if IsControlPressed(0, 21) and IsControlPressed(0, 74) then
             if voice.current == 0 then
                 voiceS = voice.default
             elseif voice.current == 1 then
@@ -52,6 +48,4 @@ Citizen.CreateThread(function()
     end
 end)
 
-function Marker(type, x, y, z, voiceS)
-     DrawMarker(type, x, y, z - 1.2, 0.0, 0.0, 0.0, 0, 0.0, 0.0, voiceS, voiceS, 1.0, r, g, b, a, false, true, 2, false, false, false, false)
-end
+
